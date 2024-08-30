6e3794615a4474d8c404418d74c77a9155ca0937
awancs <gunawancs938@gmail.com>
2024-08-30 14:41:13 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+let hari = new Date().getDay()
+
+switch(hari){
+    case 1:
+        hari = "Senin"
+    break
+    case 2:
+        hari = "Selasa"
+    break
+    case 3:
+        hari = "Rabu"
+    break
+    case 4:
+        hari = "Kamis"
+    break
+    case 5:
+        hari = "Jumat"
+    break
+    case 6:
+        hari = "Sabtu"
+    break
+    case 7:
+        hari = "Minggu"
+    break
+}
+alert(`hari ini adalah hari ${hari}`)
+
+
+
+
+
+
+
+
+
